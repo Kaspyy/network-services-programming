2dd6abf46240e8b242ebbb8435788af3da2d6b8d
Kacper <kpawlak2000@gmail.com>
2022-03-28 12:57:23 +0200

    lab 4 task 2

---
+++ PUS-04-Protokol_SCTP-Linux/src/client2.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/sctp.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 256
+
+int main(int argc, char **argv)
+{
+    int sockfd;
+    int retval, flags, slen;
+    struct addrinfo hints, *result;
+    char buff[BUFF_SIZE];
+    struct sctp_initmsg initmsg;
+    struct sctp_sndrcvinfo sndrcvinfo;
+    struct sctp_event_subscribe events;
+    struct sctp_status status;
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "Invocation: %s <IP ADDRESS> <PORT NUMBER>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC; /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;
+
+    retval = getaddrinfo(argv[1], argv[2], &hints, &result);
+
+    if (retval != 0)
+    {
+        fprintf(stderr, "getaddrinfo(): %s\n", gai_strerror(retval));
+        exit(EXIT_FAILURE);
+    }
+
+    if (result == NULL)
+    {
+        fprintf(stderr, "Could not connect!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Tworzenie gniazda */
+    sockfd = socket(result->ai_family, result->ai_socktype, IPPROTO_SCTP);
+
+    if (sockfd == -1)
+    {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&initmsg, 0, sizeof(initmsg));
+    /* Określenie liczby strumieni wychodzących */
+    initmsg.sinit_num_ostreams = 3;
+    /* Określenie liczby strumieni przychodzących */
+    initmsg.sinit_max_instreams = 4;
+    /* Określenie liczby prób nawiązania asocjacji */
+    initmsg.sinit_max_attempts = 5;
+    retval = setsockopt(sockfd, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg));
+    if (retval != 0)
+
+    {
+        perror("setsockopt()");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(sockfd, result->ai_addr, result->ai_addrlen) == -1)
+    {
+        perror("connect()");
+        exit(EXIT_FAILURE);
+    }
+
+    freeaddrinfo(result);
+
+    memset(&events, 0, sizeof(events));
+    events.sctp_data_io_event = 1;
+    retval = setsockopt(sockfd, SOL_SCTP, SCTP_EVENTS, (const void *)&events, sizeof(events));
+
+    slen = sizeof(status);
+    retval = getsockopt(sockfd, SOL_SCTP, SCTP_STATUS, &status, (socklen_t *)&slen);
+
+    /* Wypisanie informacji o asocjacji */
+    printf("ID: %d\n", status.sstat_assoc_id);
+    printf("STATE: %d\n", status.sstat_state);
+    printf("INSTREAMS: %d\n", status.sstat_instrms);
+    printf("OUTSTREAMS: %d\n", status.sstat_outstrms);
+
+    /* Odbieranie wiadomości */
+    for (int i = 0; i < initmsg.sinit_num_ostreams - 1; i++)
+    {
+        memset(buff, 0, sizeof(buff));
+        /* sctp_recvmsg() odbiera wiadomość z gniazda */
+        retval = sctp_recvmsg(sockfd, buff, BUFF_SIZE, (struct sockaddr *)NULL, 0, &sndrcvinfo, &flags);
+
+        if (retval > 0)
+        {
+            buff[retval] = 0;
+            printf("(STREAM: %d) %s\n", sndrcvinfo.sinfo_stream, buff);
+            fflush(stdout);
+        }
+    }
+    close(sockfd);
+    exit(EXIT_SUCCESS);
+}
---
+++ PUS-04-Protokol_SCTP-Linux/src/server2.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/sctp.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <time.h>
+
+#define BUFF_SIZE 256
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd;
+    int retval;
+    struct sockaddr_in servaddr;
+    char buffer[BUFF_SIZE];
+    struct sctp_initmsg initmsg;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Invocation: %s <PORT NUMBER>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    listenfd = socket(PF_INET, SOCK_STREAM, IPPROTO_SCTP);
+    if (listenfd == -1)
+    {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Ustalanie liczby strumieni wychodzących i przychodzących */
+    memset(&initmsg, 0, sizeof(initmsg));
+    initmsg.sinit_num_ostreams = 2;
+    initmsg.sinit_max_instreams = 2;
+    retval = setsockopt(listenfd, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg));
+    if (retval == -1)
+    {
+        perror("setsockopt()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Ustalanie adresu IP i numeru portu */
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(atoi(argv[1]));
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
+    {
+        perror("bind()");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(listenfd, 5) == -1)
+    {
+        perror("listen()");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Odbieranie połączeń */
+    while (1)
+    {
+        connfd = accept(listenfd, NULL, 0);
+        if (connfd == -1)
+        {
+            perror("accept()");
+            exit(EXIT_FAILURE);
+        }
+
+        /* Pobieranie daty i czasu */
+        time_t t = time(NULL);
+        struct tm *tm = localtime(&t);
+
+        /* Wysłanie daty  */
+        memset(buffer, 0, sizeof(buffer));
+        sprintf(buffer, "%d-%d-%d", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday);
+        retval = sctp_sendmsg(connfd, buffer, strlen(buffer), NULL, 0, 0, 0, 0, 0, 0);
+        if (retval == -1)
+        {
+            perror("sctp_sendmsg()");
+            exit(EXIT_FAILURE);
+        }
+
+        /* Wysyłanie czasu */
+        memset(buffer, 0, sizeof(buffer));
+        sprintf(buffer, "%d:%d:%d", tm->tm_hour, tm->tm_min, tm->tm_sec);
+        retval = sctp_sendmsg(connfd, buffer, strlen(buffer), NULL, 0, 0, 0, 1, 0, 0);
+        if (retval == -1)
+        {
+            perror("sctp_sendmsg()");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
